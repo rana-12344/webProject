cba9d7fd9f24e4010e93f4e04c060479abbfa2fb
rana-12344 <74002474+rana-12344@users.noreply.github.com>
2020-11-07 14:33:44 -0800

    Update lab10.php

--- lab10.php
+++ lab10.php
@@ -1,5 +1,5 @@
 <?php 
-$con=mysqli_connect("localhost","root","","products") 
+$con=mysqli_connect("localhost","root","","prod") 
 or die (mysqli_connection_error());
 echo "Connected to MySQL <br>";
 
