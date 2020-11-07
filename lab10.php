4c66afacb8c5af4e26889d532f49f095c8f0ef58
RaghdM12 <73111193+RaghdM12@users.noreply.github.com>
2020-11-08 01:08:34 +0300

    Add files via upload

---
+++ lab10.php
@@ -0,0 +1,35 @@
+<?php 
+$con=mysqli_connect("localhost","root","","products") 
+or die (mysqli_connection_error());
+echo "Connected to MySQL <br>";
+
+mysqli_query($con, " CREATE TABLE NOT WXISTS 
+products_info(
+p_id INT NOT NULL AUTO_INCREMENT,PRIMARY KEY(p_id),p_name varchar(30),p_price INT )")
+or die(Mysqli_Connect_error());
+echo "table created! <br>";
+
+mysqli_query($con,"INSERT INTO products_info (p_name  ,p_price)VALUED('Blactberry','1900')")
+or die (Mysqli_Connect_error());
+
+mysqli_query($con,"INSERT INTO products_info (p_name  ,p_price)VALUED('Galaxy Note3','2300')")
+or die (Mysqli_Connect_error());
+
+mysqli_query($con,"INSERT INTO products_info (p_name  ,p_price)VALUED('iphone 6s','2700')")
+or die (Mysqli_Connect_error());
+
+$query="SELECT *FROM products_info";
+$result = mysqli_query($con,$query) or die (Mysqli_Connect_error());
+
+while ($row=mysqli_fetch_array($result,MYSQLI_ASSOC))
+{
+	echo $row['p_name']."-".$row['p_price'];
+	echo "<br>";
+}
+
+
+mysqli_query($con,"INSERT INTO products_info (p_name  ,p_price)VALUED('Sony Experia 22','1350')")
+or die (Mysqli_Connect_error());
+
+$res=mysqli_query($con,"UPDATE products_info SET p_price=1600 Where p_name='BLACHBERRY'")
+or die (Mysqli_Connect_error());
